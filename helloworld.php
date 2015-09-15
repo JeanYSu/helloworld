3ee8e12e20bfcdc114c64aae8861ca6b5b253be5
JeanYSu <ysu@g.hardvard.edu>
2015-09-15 15:30:34 -0700

    first commit

---
+++ helloworld.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Hello World</title>
+	<meta charset='utf-8'>
+
+</head>
+<body>
+
+	<?php
+	echo 'Hello World! <br>You are visiting ';
+	echo $_SERVER['SERVER_NAME'];
+	echo '<br>This is Jean Su!';
+	?>
+
+</body>
+</html>
